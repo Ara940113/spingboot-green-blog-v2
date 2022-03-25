7256abd454f20f5c23dbacd090415897c9293ade
AR <aki1013@naver.com>
2022-03-25 12:32:00 +0900

    11. 회원정보보기 완료 -throw처리

--- src/main/java/site/metacoding/blogv2/service/UserService.java
+++ src/main/java/site/metacoding/blogv2/service/UserService.java
@@ -1,5 +1,7 @@
 package site.metacoding.blogv2.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,24 +11,36 @@ import site.metacoding.blogv2.domain.user.UserRepository;
 import site.metacoding.blogv2.web.api.dto.user.JoinDto;
 import site.metacoding.blogv2.web.api.dto.user.LoginDto;
 
+//  웹브라우저 -> 컨트롤러 -> 서비스 -> 레파지토리 -> 영속성컨텍스트 -> DB
+
 @RequiredArgsConstructor
-@Service // 컴포넌트 스캔시에 IoC 컨테이너에 등록됨 // 트랜잭션 관리하는 오브젝트임. 기능을 넣어두는 모임
+@Service // 컴포넌트 스캔시에 IoC 컨테이너에 등록됨 // 트랜잭션 관리하는 오브젝트임
 public class UserService {
 
     private final UserRepository userRepository;
 
+    public User 회원정보(Integer id) {
+        Optional<User> userOp = userRepository.findById(id);
+
+        if (userOp.isPresent()) {
+            return userOp.get();
+        } else {
+            throw new RuntimeException("아이디를 찾을 수 없습니다.");
+        }
+
+    }
+
     @Transactional
     public void 회원가입(JoinDto joinDto) {
-        // save하면 db에 insert하고 insert된 결과를 다시 return해준다
-
+        // save하면 db에 insert하고 insert된 결과를 다시 return 해준다.
         userRepository.save(joinDto.toEntity());
-
     }
 
     public User 로그인(LoginDto loginDto) {
         // 로그인 처리 쿼리를 JPA에서 제공해주지 않는다.
-
+        // SELECT * FROM user WHERE username = :username AND password = :password
         User userEntity = userRepository.mLogin(loginDto.getUsername(), loginDto.getPassword());
         return userEntity;
     }
+
 }
