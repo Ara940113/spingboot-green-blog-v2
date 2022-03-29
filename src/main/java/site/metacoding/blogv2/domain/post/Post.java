435498a3215b4e43eaf9c7a5c217ea3e8b8736d5
AR <aki1013@naver.com>
2022-03-29 11:48:31 +0900

    14. 댓글 엔티티 생성 - 양방향 매핑하기

--- src/main/java/site/metacoding/blogv2/domain/post/Post.java
+++ src/main/java/site/metacoding/blogv2/domain/post/Post.java
@@ -1,6 +1,7 @@
 package site.metacoding.blogv2.domain.post;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,6 +13,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -19,8 +23,13 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import site.metacoding.blogv2.domain.comment.Comment;
 import site.metacoding.blogv2.domain.user.User;
 
+/**
+ * GET /post/1/상세보기
+ * User,Post,LIst<Comment>
+ */
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -39,9 +48,17 @@ public class Post { // N (드라이빙 테이블, FK의 주인)
     @Column(nullable = false)
     private String content;
 
+    @ColumnDefault("0")
+    @Column(nullable = false)
+    private Integer pageCount;
+    // 조회수 : 누가 들어왔는지 체크를 한다면 테이블을 빼야하지만 데이터 하나로 끝낼수 있으면 테이블을 만들지 않아도 된다
+
     @JoinColumn(name = "userId")
     @ManyToOne(fetch = FetchType.EAGER)
     private User user;
+
+    @OneToMany(mappedBy = "post") // 연관관계의 주인의 변수명
+    private List<Comment> comments; // 양방향 맵핑 , lazy로 두는게 좋다. 게시글 목록을 뿌릴 때는 다 들고 올 필요가 없기 때문에
 
     @CreatedDate
     private LocalDateTime createdate;
